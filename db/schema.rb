757ba2e809fb5ff5306baf46717eddb06ed6d1e2
Kevin Lenehan <klenehan123@gmail.com>
2021-08-03 16:13:09 +0100

    postgres db

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2021_06_22_183440) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "event_attendings", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "attendee_id"
-    t.integer "attended_event_id"
+    t.bigint "attendee_id"
+    t.bigint "attended_event_id"
     t.integer "status"
     t.index ["attended_event_id"], name: "index_event_attendings_on_attended_event_id"
     t.index ["attendee_id"], name: "index_event_attendings_on_attendee_id"
@@ -27,7 +30,7 @@ ActiveRecord::Schema.define(version: 2021_06_22_183440) do
     t.date "event_on"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "event_creator_id"
+    t.bigint "event_creator_id"
     t.string "name"
     t.index ["event_creator_id"], name: "index_events_on_event_creator_id"
   end
